9a7b7ba0fef6afa592272c9eda13338f34e655ba
Hamza <karlchurchill@gmail.com>
2018-03-09 01:40:23 +0000

    Merging changes.

--- projects/test/newlist/application.cpp
+++ projects/test/newlist/application.cpp
@@ -18,48 +18,48 @@ using namespace EXNGPrivate;
 
 class NewListTestApp : public Application
 {
-	public:
-		sint32 run();
+  public:
+    sint32 run();
 
-	public:
-		NewListTestApp();
-		~NewListTestApp();
-	private:
+  public:
+    NewListTestApp();
+    ~NewListTestApp();
+  private:
 };
 
 Application* Application::createInstance()
 {
-	return new NewListTestApp();
+  return new NewListTestApp();
 }
 
 void Application::destroyInstance(Application* app)
 {
-	delete app;
+  delete app;
 }
 
 NewListTestApp::NewListTestApp()
 {
-	SystemLog::write(SystemLog::INFO, "Created NewListTestApp\n");
+  SystemLog::write(SystemLog::INFO, "Created NewListTestApp\n");
 
 }
 
 NewListTestApp::~NewListTestApp()
 {
-	SystemLog::write(SystemLog::INFO,"Destroyed NewListTestApp\n");
+  SystemLog::write(SystemLog::INFO,"Destroyed NewListTestApp\n");
 }
 
 sint32 NewListTestApp::run()
 {
-	static VLinkCache::VLinkNode* node[64];
+  static VLinkCache::VLinkNode* node[64];
 
-	for (sint32 i=0; i<64; i++) {
-		node[i] = VLinkCache::createLink();
-		if (node[i]) {
-			node[i]->item = this;
-		}
-		//if ((i%3)==0) {
-			VLinkCache::destroyLink(node[i]);
-		//}
-	}
-	return 0;
+  for (sint32 i=0; i<64; i++) {
+    node[i] = VLinkCache::createLink();
+    if (node[i]) {
+      node[i]->item = this;
+    }
+    //if ((i%3)==0) {
+      VLinkCache::destroyLink(node[i]);
+    //}
+  }
+  return 0;
 }
--- projects/test/newlist/reflist.hpp
+++ projects/test/newlist/reflist.hpp
@@ -5,43 +5,43 @@
 namespace EXNGPrivate {
 
 class VLinkCache {
-	//protected:
-	public:
-		enum {
-			LINK_CACHEUNIT_SIZE	= 8
-		};
+  //protected:
+  public:
+    enum {
+      LINK_CACHEUNIT_SIZE = 8
+    };
 
-		// basic node definition
-		struct VLinkNode {
-			VLinkNode*	prev;
-			VLinkNode*	next;
-			VLinkNode*	nextFree;
-			void*				item;			// payload, null implies node is unused
-		};
+    // basic node definition
+    struct VLinkNode {
+      VLinkNode*  prev;
+      VLinkNode*  next;
+      VLinkNode*  nextFree;
+      void*       item;     // payload, null implies node is unused
+    };
 
-	public:
-		static VLinkNode*		createLink();
-		static void					destroyLink(VLinkNode* v);
-		static uint32				expungeCache();
+  public:
+    static VLinkNode*   createLink();
+    static void         destroyLink(VLinkNode* v);
+    static uint32       expungeCache();
 
 
-	private:
-		struct VLinkBlock {
-			// simple chained block of LINK_CACHE_UNIT VLinkNode is used to implement
-			// a dynamically sizeable cache of VLinkNode instances
-			VLinkBlock*	prev;
-			VLinkBlock*	next;
-			VLinkNode		nodes[LINK_CACHEUNIT_SIZE];
-		};
+  private:
+    struct VLinkBlock {
+      // simple chained block of LINK_CACHE_UNIT VLinkNode is used to implement
+      // a dynamically sizeable cache of VLinkNode instances
+      VLinkBlock* prev;
+      VLinkBlock* next;
+      VLinkNode   nodes[LINK_CACHEUNIT_SIZE];
+    };
 
-		static VLinkBlock*	head;
-		static VLinkNode*		nextFreeNode;
-		static uint32				totalNodesUsed;
-		static uint32				totalNodesAllocated;
-		// internal cache management routines
-		static void					destroyCache();
-		static bool					expandCache();
-		static uint32				blockUsage(VLinkBlock* block);
+    static VLinkBlock*  head;
+    static VLinkNode*   nextFreeNode;
+    static uint32       totalNodesUsed;
+    static uint32       totalNodesAllocated;
+    // internal cache management routines
+    static void         destroyCache();
+    static bool         expandCache();
+    static uint32       blockUsage(VLinkBlock* block);
 };
 
 
